4e6a476ba877bb0ccdf380b111a40410fe13db4a
legendbabs <tmsalami@student.lautech.edu.ng>
2020-11-27 18:52:12 +0100

    initial commit

---
+++ read_write_files.py
@@ -0,0 +1,39 @@
+import os
+
+my_path = r'C:\Users\USER\Desktop\chess24WebPage\json_module.py'
+
+print(os.path.exists(my_path))
+
+print(os.path.isfile(my_path))
+
+print(os.path.isdir(my_path))
+
+# print(os.path.getsize(my_path))
+
+# print(os.listdir(my_path))
+
+# total_size = 0
+
+# for filename in os.listdir(my_path):
+# 	total_size += os.path.getsize(os.path.join(my_path, filename))
+
+# print(total_size)
+
+# print(my_path)
+
+# print(os.path.dirname(my_path))
+
+# print(os.path.basename(my_path))
+
+# print(os.path.split(my_path))
+
+# print(my_path.split(os.path.sep))
+
+
+# print(os.path.abspath('.'))
+
+# print(os.path.isabs(os.path.abspath('.')))
+
+# print(os.path.relpath(r'C:\Users\USER\Desktop\chess24WebPage', r'C:\Users\USER'))
+
+# print(os.path.relpath(r'C:\Users\USER\Desktop\chess24WebPage', r'C:\spam\eggs'))
